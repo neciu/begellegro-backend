1f11c75378a1b0c5230e358ada98069884ed3e87
Tomasz Netczuk <netczuk.tomasz@gmail.com>
2015-03-14 17:25:27 +0100

    Bla bla bla.

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ app.use(bodyParser.json());
 var md5 = require('MD5');
 var db = require('./data.json');
 
+var UrbanAirshipPush = require('urban-airship-push');
+var config = {
+    key: 'Yori1SnMSb29IuW559md_w',
+    secret: 'R1ynS3rSQnet_mSJg0Qttg',
+    masterSecret: 'XKjWkn2nSquZgG4CzTZomQ'
+};
+var urbanAirship = new UrbanAirshipPush(config);
+
 app.get('/', function (req, res) {
     res.send('Hello World!')
 });
@@ -16,7 +24,7 @@ function findObjectById(collection, id) {
 }
 
 app.post('/push-tokens', function (req, res) {
-    ['email', 'token'].map(function (key) {
+    ['email', 'token', 'platform'].map(function (key) {
         if (!req.body.hasOwnProperty(key)) {
             res.status(400);
             res.send('Missing key: ' + key);
@@ -31,7 +39,7 @@ app.post('/push-tokens', function (req, res) {
         res.send('No friend for email: ' + req.body.email);
     }
 
-    var pushToken = db.pushTokens.filter(function(pushToken){
+    var pushToken = db.pushTokens.filter(function (pushToken) {
         return pushToken.userId == friend.id;
     })[0];
     if (pushToken) {
@@ -39,12 +47,15 @@ app.post('/push-tokens', function (req, res) {
     } else {
         db.pushTokens.push({
             userId: friend.id,
-            token: req.body.token
+            token: req.body.token,
+            platform: req.body.platform
         });
     }
-    pushToken = db.pushTokens.filter(function(pushToken){
+    pushToken = db.pushTokens.filter(function (pushToken) {
         return pushToken.userId == friend.id;
     })[0];
+
+    console.log('\nRecieved push token: ' + req.body.token + ' email: ' + req.body.email + ' user id: ' + friend.id + ' platform: ' + req.body.platform);
 
     res.send(pushToken);
 });
@@ -67,12 +78,14 @@ app.get('/friends', function (req, res) {
 });
 
 app.post('/begs', function (req, res) {
-    ['auctionId', 'friendsId'].map(function (key) {
+    ['userId', 'auctionId', 'friendIds'].map(function (key) {
         if (!req.body.hasOwnProperty(key)) {
             res.status(400);
             res.send('Missing key: ' + key);
         }
     });
+
+    var beggar = findObjectById('friends', req.body.userId);
 
     var auction = findObjectById('auctions', req.body.auctionId);
     if (!auction) {
@@ -81,7 +94,7 @@ app.post('/begs', function (req, res) {
         return;
     }
 
-    req.body.friendsId.map(function (friendId) {
+    req.body.friendIds.map(function (friendId) {
         var friend = findObjectById('friends', friendId);
         if (!friend) {
             res.status(404);
@@ -89,7 +102,7 @@ app.post('/begs', function (req, res) {
         }
     });
 
-    req.body.friendsId.map(function (friendId) {
+    req.body.friendIds.map(function (friendId) {
         for (var i = 0; i < db.begs.length; i++) {
             var beg = db.begs[i];
             if (beg.auctionId == req.body.auctionId && beg.friendId == friendId) {
@@ -99,7 +112,8 @@ app.post('/begs', function (req, res) {
         db.begs.push({
             id: db.begs.length,
             auctionId: req.body.auctionId,
-            friendId: friendId
+            friendId: friendId,
+            beggarId: req.body.userId
         });
     });
 
@@ -107,7 +121,88 @@ app.post('/begs', function (req, res) {
         return beg.auctionId == req.body.auctionId;
     });
 
+    req.body.friendIds.map(function (friendId) {
+        console.log('\nTrying to push to user with id: ' + friendId);
+
+        var pushToken = db.pushTokens.filter(function (pushToken) {
+            return pushToken.userId == friendId;
+        })[0];
+
+        if (pushToken) {
+            console.log('Push token found for user with id: ' + friendId);
+
+            var audience;
+            var deviceTypes;
+            if (pushToken.platform == 'android') {
+                audience = {"android_channel": pushToken.token};
+                deviceTypes = ["android"];
+            }
+
+            var pushInfo = {
+                device_types: deviceTypes,
+                audience: audience,
+                notification: {
+                    alert: 'Give ' + auction.price/100.0 + 'zł to ' + beggar.name + ' for ' + auction.title + '?',
+                    android: {
+                        extra: {
+                            request : JSON.stringify({
+                                beggar: beggar,
+                                donor: findObjectById('friends', friendId),
+                                auction: auction,
+                                numberOfDonors: req.body.friendIds.length
+                            })
+                        }
+                    }
+                }
+            };
+
+            console.log('Pushing stuff to user with id: ' + friendId + ' platform: ' + pushToken.platform);
+            urbanAirship.push.send(pushInfo, function (err, data) {
+            });
+        } else {
+            console.log('No push token for user with id: ' + friendId);
+        }
+    });
+
+    res.status(200);
     res.send(begs);
+});
+
+
+app.post('/donations', function (req, res) {
+    //donatorId
+    //auctionId
+    //amount
+
+    //var begs = db.begs.filter(function (beg) {
+    //    return beg.auctionId == req.body.auctionId;
+    //});
+    //
+    //for (var i = 0; i < begs.length; ++i) {
+    //    var beg = begs[i];
+    //    if (beg.friendId == req.body.donatorId) {
+    //        beg.donated = true;
+    //    }
+    //}
+
+
+
+    var pushInfo = {
+        "audience": "all",
+        "notification": {
+            "alert": "Alek Piotrowski has founded your item."
+        },
+        "device_types" : "all"
+    };
+
+    urbanAirship.push.send(pushInfo, function (err, data) {
+        console.log(err);
+        console.log(data    );
+    });
+
+    console.log('lol');
+
+    res.sendStatus(200);
 });
 
 
